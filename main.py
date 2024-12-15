9d7c29fe5da73c469bbf358144812357fe470810
AKKuhwah <ahkawkuh.1@gmail.com>
2024-12-14 20:59:39 -0800

    changes made

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from logic import *
 
 def main():
+    """
+    Creates and runs PyQt6
+
+    This function sets up the QApplication, creates a Logic window,
+    and starts the event loop.
+    """
     application = QApplication([])
     window = Logic()
     window.show()
